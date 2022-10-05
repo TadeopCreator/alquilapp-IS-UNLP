7d5610f590c0a07e7fcf42e213fcb34d592d7709
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2022-10-04 22:25:26 -0300

    Home view and controller created

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+  end
+end
+
+# rails g controller home index    to generate a new view and a controller
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #get 'home/index'
+
+  # rails routes to seee all the routes in the app
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
