b5b4b61dcfb93d27efacb2047c1ef141568eeb6c
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2022-10-26 00:41:55 -0300

    user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:user, :supervisor, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+       
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
+
+# admin@admin.com admin123
+# supervisor@supervisor.com supervisor123
+# user@user.com user123
