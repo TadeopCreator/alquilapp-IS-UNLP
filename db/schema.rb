f3aaa26fb51018d18b5b049dd5d5ffe8b1c78edc
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2022-11-01 18:31:49 -0300

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_000001) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_01_204259) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,26 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_000001) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "patente"
-    t.datetime "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "supervisors", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "surname"
-    t.string "dni"
-    t.string "email"
-    t.boolean "habilitado"
-    t.boolean "borrado"
-  end
-  
   create_table "autos", force: :cascade do |t|
     t.integer "num_rel"
     t.string "patente"
@@ -94,9 +73,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_000001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
-    t.integer "id_rol"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string "name"
+    t.string "lastname"
+    t.integer "dni"
+    t.string "contact"
+    t.decimal "lon"
+    t.decimal "lat"
+    t.boolean "deleted"
+    t.boolean "enable"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
