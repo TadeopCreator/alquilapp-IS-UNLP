a12a3a7adc7a78f8cfa3b600e296e072b1f0f6df
InfinioKneza <luciocanessa@gmail.com>
2022-11-10 23:08:05 -0300

    Arreglado el tirar error por formato incorrecto de insertar licencia~

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_11_10_223254) do
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_223254) do
     t.string "name"
     t.string "lastname"
     t.string "dni"
-    t.string "email"
+    t.string "mail"
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -133,4 +131,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_223254) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
